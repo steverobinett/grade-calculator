9352807cf866d102b2833df2fd4132ee0de98d85
Steven Robinett <Steven.robinett@gfcmsu.edu>
2026-03-24 07:43:40 -0600

    Add calculate_average function to compute average scores

---
+++ grades.py
@@ -0,0 +1,9 @@
+# grades.py
+def calculate_average(scores):
+    """Return the average of a list of scores."""
+    return sum(scores) / len(scores)
+ 
+if __name__ == '__main__':
+    scores = [88, 92, 75, 100, 63]
+    print(f"Average: {calculate_average(scores)}")
+
